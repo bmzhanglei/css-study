a80696ccf67a41521c15726d67ccae4c016e76c5
bmzhanglei <115097376@qq.com>
2021-03-21 17:13:13 +0800

    css3

---
+++ css3/test.js
@@ -0,0 +1,22 @@
+(function(window, document) {
+
+    function go(num) {
+    }
+  
+    function handleEvents() {
+    }
+  
+    function initialize() {
+        return 'initialize';
+    }
+  
+    function dieCarouselDie() {
+    }
+  
+    //attach to the global scope
+    window.finalCarousel = {
+      init : initialize,
+      destroy : dieCarouselDie
+    }
+  
+  })(  window, document );
